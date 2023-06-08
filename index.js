b03b01281a520bdcb7d5e6411aca9c28a9509ac6
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-06-08 16:30:24 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron')
+
+
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})

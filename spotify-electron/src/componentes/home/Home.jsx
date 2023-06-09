d8cc2581539299a8ffec82c18c9b5c0cec5c45a6
Fernando <fernando.morell.teec@gmail.com>
2023-06-09 16:16:59 +0200

    Update pantalla Home

--- spotify-electron/src/componentes/home/Home.jsx
+++ spotify-electron/src/componentes/home/Home.jsx
@@ -2,5 +2,24 @@ import React from "react";
 import styles from "./homeCss.module.css";
 
 export default function Home() {
-    return <div className={`container-fluid ${styles.clase}`}>hola</div>;
+
+
+    return (
+
+        <div className={`container-fluid ${styles.clase}`}>
+
+            <div className={`${styles.card}`}>
+                <img src="..." className={`card-img-top`}/>
+                    <div className={`card-body`}>
+                        <h5 className={`card-title`}>Quedate</h5>
+                        <p className={`card-text`}>Quevedo</p>
+                    </div>
+            </div>
+
+
+        </div>
+
+    );
+
+
 }

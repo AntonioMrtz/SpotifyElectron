7dfdf1be7d6efa26abbb38666c1ae54c44a73785
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-06-09 16:15:52 +0200

    carpeta assets creada

--- spotify-electron/src/componentes/home/Home.jsx
+++ spotify-electron/src/componentes/home/Home.jsx
@@ -2,5 +2,22 @@ import React from "react";
 import styles from "./homeCss.module.css";
 
 export default function Home() {
-    return <div className={`container-fluid ${styles.clase}`}>hola</div>;
+    return (
+
+
+
+
+        <div className={`container-fluid ${styles.clase}`}>hola</div>
+
+
+
+
+
+
+
+    )
+    
+    
+    
+    
 }

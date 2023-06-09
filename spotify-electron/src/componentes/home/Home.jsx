b491ff4eabf4ec532c9e72511613dbac4e3cc805
Fernando <fernando.morell.teec@gmail.com>
2023-06-09 18:02:25 +0200

    Terminado tarjetas y listas del Home

--- spotify-electron/src/componentes/home/Home.jsx
+++ spotify-electron/src/componentes/home/Home.jsx
@@ -6,118 +6,144 @@ export default function Home() {
 
     return (
 
-        <div className={`container-fluid d-flex flex-column ${styles.column}`}>
+        <div className={`container-fluid d-flex flex-column ${styles.principal}`}>
             
-            <div className={`container-fluid d-flex flex-row ${styles.row}`}>
+            <div className={`container-fluid d-flex flex-column ${styles.columnOfListas}`}>
+
+                <header className={`container-fluid d-flex flex-row ${styles.columnHead}`}>
+                    <div className={`container-fluid d-flex ${styles.columnTitle}`}>
+                        <h4>Titulo</h4>
+                    </div>
+                    <div className={`container-fluid d-flex ${styles.mostrarT}`}>
+                        <p>Mostrar todos</p>
+                    </div>
+                </header>
+
+                <section className={`container-fluid d-flex flex-row ${styles.row}`}>
+                
+                    <div className={`rounded ${styles.card}`}>
+                        <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
+                            <div className={`${styles.card_body}`}>
+                                <h5 className={`${styles.tituloLista}`}>Quedate</h5>
+                                <p className={`${styles.autorLista}`}>Quevedo</p>
+                            </div>
+                    </div>
+
+                    <div className={`rounded ${styles.card}`}>
+                        <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
+                            <div className={`${styles.card_body}`}>
+                                <h5 className={`${styles.tituloLista}`}>Quedate</h5>
+                                <p className={`${styles.autorLista}`}>Quevedo</p>
+                            </div>
+                    </div>
+
+                    
+
+                    
+
+
+                </section>
+            </div>
+
+            <div className={`container-fluid d-flex flex-column ${styles.columnOfListas}`}>
+
+                <div className={`container-fluid d-flex flex-row ${styles.columnHead}`}>
+                    <h4 className={`${styles.columnTitle}`}>
+                        Titulo
+                    </h4>
+                    <p className={`${styles.mostrarT}`}>
+                        Mostrar todos
+                    </p>
+                </div>
+
+                <div className={`container-fluid d-flex flex-row ${styles.row}`}>
+                
+                <div className={`rounded ${styles.card}`}>
+                        <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
+                            <div className={`${styles.card_body}`}>
+                                <h5 className={`${styles.tituloLista}`}>Quedate</h5>
+                                <p className={`${styles.autorLista}`}>Quevedo</p>
+                            </div>
+                    </div>
+
+                    <div className={`rounded ${styles.card}`}>
+                        <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
+                            <div className={`${styles.card_body}`}>
+                                <h5 className={`${styles.tituloLista}`}>Quedate</h5>
+                                <p className={`${styles.autorLista}`}>Quevedo</p>
+                            </div>
+                    </div>
+
+                    <div className={`rounded ${styles.card}`}>
+                        <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
+                            <div className={`${styles.card_body}`}>
+                                <h5 className={`${styles.tituloLista}`}>Quedate</h5>
+                                <p className={`${styles.autorLista}`}>Quevedo</p>
+                            </div>
+                    </div>
+
+                    <div className={`rounded ${styles.card}`}>
+                        <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
+                            <div className={`${styles.card_body}`}>
+                                <h5 className={`${styles.tituloLista}`}>Quedate</h5>
+                                <p className={`${styles.autorLista}`}>Quevedo</p>
+                            </div>
+                    </div>
+
+
+                </div>
+            </div>
+
+            <div className={`container-fluid d-flex flex-column ${styles.columnOfListas}`}>
+
+                <div className={`container-fluid d-flex flex-row ${styles.columnHead}`}>
+                    <h4 className={`${styles.columnTitle}`}>
+                        Titulo
+                    </h4>
+                    <p className={`${styles.mostrarT}`}>
+                        Mostrar todos
+                    </p>
+                </div>
+
+                <div className={`container-fluid d-flex flex-row ${styles.row}`}>
+                
+                <div className={`rounded ${styles.card}`}>
+                        <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
+                            <div className={`${styles.card_body}`}>
+                                <h5 className={`${styles.tituloLista}`}>Quedate</h5>
+                                <p className={`${styles.autorLista}`}>Quevedo</p>
+                            </div>
+                    </div>
+
+                    <div className={`rounded ${styles.card}`}>
+                        <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
+                            <div className={`${styles.card_body}`}>
+                                <h5 className={`${styles.tituloLista}`}>Quedate</h5>
+                                <p className={`${styles.autorLista}`}>Quevedo</p>
+                            </div>
+                    </div>
+
+                    <div className={`rounded ${styles.card}`}>
+                        <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
+                            <div className={`${styles.card_body}`}>
+                                <h5 className={`${styles.tituloLista}`}>Quedate</h5>
+                                <p className={`${styles.autorLista}`}>Quevedo</p>
+                            </div>
+                    </div>
+
+                    <div className={`rounded ${styles.card}`}>
+                        <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
+                            <div className={`${styles.card_body}`}>
+                                <h5 className={`${styles.tituloLista}`}>Quedate</h5>
+                                <p className={`${styles.autorLista}`}>Quevedo</p>
+                            </div>
+                    </div>
+
+
+                </div>
+            </div>
+
             
-            <div className={`rounded ${styles.card}`}>
-                <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
-                    <div className={`${styles.card_body}`}>
-                        <h5 className={`card-title`}>Quedate</h5>
-                        <p className={`card-text`}>Quevedo</p>
-                    </div>
-            </div>
-
-            <div className={`rounded ${styles.card}`}>
-                <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
-                    <div className={`${styles.card_body}`}>
-                        <h5 className={`card-title`}>Quedate</h5>
-                        <p className={`card-text`}>Quevedo</p>
-                    </div>
-            </div>
-
-            <div className={`rounded ${styles.card}`}>
-                <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
-                    <div className={`${styles.card_body}`}>
-                        <h5 className={`card-title`}>Quedate</h5>
-                        <p className={`card-text`}>Quevedo</p>
-                    </div>
-            </div>
-
-            <div className={`rounded ${styles.card}`}>
-                <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
-                    <div className={`${styles.card_body}`}>
-                        <h5 className={`card-title`}>Quedate</h5>
-                        <p className={`card-text`}>Quevedo</p>
-                    </div>
-            </div>
-
-
-            </div>
-
-            <div className={`container-fluid d-flex flex-row ${styles.row}`}>
-            
-            <div className={`rounded ${styles.card}`}>
-                <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
-                    <div className={`${styles.card_body}`}>
-                        <h5 className={`card-title`}>Quedate</h5>
-                        <p className={`card-text`}>Quevedo</p>
-                    </div>
-            </div>
-
-            <div className={`rounded ${styles.card}`}>
-                <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
-                    <div className={`${styles.card_body}`}>
-                        <h5 className={`card-title`}>Quedate</h5>
-                        <p className={`card-text`}>Quevedo</p>
-                    </div>
-            </div>
-
-            <div className={`rounded ${styles.card}`}>
-                <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
-                    <div className={`${styles.card_body}`}>
-                        <h5 className={`card-title`}>Quedate</h5>
-                        <p className={`card-text`}>Quevedo</p>
-                    </div>
-            </div>
-
-            <div className={`rounded ${styles.card}`}>
-                <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
-                    <div className={`${styles.card_body}`}>
-                        <h5 className={`card-title`}>Quedate</h5>
-                        <p className={`card-text`}>Quevedo</p>
-                    </div>
-            </div>
-
-
-            </div>
-
-            <div className={`container-fluid d-flex flex-row ${styles.row}`}>
-            
-            <div className={`rounded ${styles.card}`}>
-                <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
-                    <div className={`${styles.card_body}`}>
-                        <h5 className={`card-title`}>Quedate</h5>
-                        <p className={`card-text`}>Quevedo</p>
-                    </div>
-            </div>
-
-            <div className={`rounded ${styles.card}`}>
-                <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
-                    <div className={`${styles.card_body}`}>
-                        <h5 className={`card-title`}>Quedate</h5>
-                        <p className={`card-text`}>Quevedo</p>
-                    </div>
-            </div>
-
-            <div className={`rounded ${styles.card}`}>
-                <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
-                    <div className={`${styles.card_body}`}>
-                        <h5 className={`card-title`}>Quedate</h5>
-                        <p className={`card-text`}>Quevedo</p>
-                    </div>
-            </div>
-
-            <div className={`rounded ${styles.card}`}>
-                <img src={require(`./quedate.jpg`)} className={`card-img-top rounded`}/>
-                    <div className={`${styles.card_body}`}>
-                        <h5 className={`card-title`}>Quedate</h5>
-                        <p className={`card-text`}>Quevedo</p>
-                    </div>
-            </div>
-
-
-            </div>
         </div>
 
 

b42c6361f6b109eb9b5a251a191dea6c5395da89
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-06-17 18:58:56 +0200

    Portada cancion , titulo y artista footer DONE

--- Electron/spotify-electron/src/componentes/footer/Footer.jsx
+++ Electron/spotify-electron/src/componentes/footer/Footer.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 import styles from "./footerCss.module.css";
 
-
 export default function Footer() {
-  return (
-    <div className={`container-fluid d-flex flex-column ${styles.wrapperFooter}`}>Footer</div>
-  )
+    return (
+        <div
+            className={`container-fluid d-flex flex-row space-evenly ${styles.wrapperFooter}`}
+        >
+            <div
+                className={`d-flex flex-row justify-content-start container-fluid ${styles.songInfoContainer}`}
+            >
+                
+              <img className="img-rounded" src={require(`./quedate.jpg`)} alt="" />
+
+              <div className={`d-flex flex-column ${styles.infoCancionContainer}`}>
+                  <a href="">Quedate</a>
+                  <a href="">Francisco Quevedo</a>
+
+              </div>
+
+              <div className={`d-flex flex-column ${styles.likeContainer}`}>
+                  <a href=""><i class="fa-regular fa-heart"></i></a>
+              </div>
+
+            </div>
+            <div
+                className={`d-flex container-fluid  justify-content-center ${styles.playerBarContainer}`}
+            >
+                hola
+            </div>
+            <div
+                className={`d-flex container-fluid justify-content-end ${styles.settingsContainer}`}
+            >
+                hola
+            </div>
+        </div>
+    );
 }

a9bd93ebfff61f9c79024165cc6d69d23203cb7f
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-07-01 15:52:15 +0200

    Song info ahora no muestra nada si no hay cancion seleccionada

--- Electron/src/componentes/footer/SongInfo/SongInfo.tsx
+++ Electron/src/componentes/footer/SongInfo/SongInfo.tsx
@@ -1,60 +1,47 @@
-import styles from "./songInfo.module.css";
-import foto from "../../../assets/imgs/quedate.jpg"
-import { useEffect, useState } from "react";
+import styles from './songInfo.module.css';
+import foto from '../../../assets/imgs/quedate.jpg';
+import { Fragment, useEffect, useState } from 'react';
 
-interface PropsSongInfo{
-
-  songInfo : JSON | undefined;
-
+interface PropsSongInfo {
+  songInfo: JSON | undefined;
 }
 
-export default function SongInfo(props:PropsSongInfo | any) {
+export default function SongInfo(props: PropsSongInfo | any) {
+  const [name, setName] = useState<string>();
+  const [thumbnail, setThumbnail] = useState<string>();
+  const [artist, setArtist] = useState<string>();
 
-    const [name,setName] = useState<string>();
-    const [thumbnail,setThumbnail] = useState<string>();
-    const [artist,setArtist] = useState<string>();
-
-
-    const updateSongInfo = async () => {
-
-      if(props.songInfo){
-
-        //console.log(props.songInfo)
-        setName(props.songInfo["name"])
-        setThumbnail(props.songInfo["photo"])
-        setArtist(props.songInfo["artist"])
-
-      };
-
+  const updateSongInfo = async () => {
+    if (props.songInfo) {
+      //console.log(props.songInfo)
+      setName(props.songInfo['name']);
+      setThumbnail(props.songInfo['photo']);
+      setArtist(props.songInfo['artist']);
     }
+  };
 
-      useEffect(() => {
-        updateSongInfo();
-      }, [props.songInfo]);
+  useEffect(() => {
+    updateSongInfo();
+  }, [props.songInfo]);
 
-
-
-    return (
-        <div
-            className={`d-flex flex-row justify-content-start container-fluid ${styles.songInfoContainer}`}
-        >
-            <img
-                src={thumbnail}
-                alt=""
-            />
-
-            <div
-                className={`d-flex flex-column ${styles.infoCancionContainer}`}
-            >
-                <a href="">{name}</a>
-                <a href="">{artist}</a>
-            </div>
-
-            <div className={`d-flex flex-column ${styles.likeContainer}`}>
-                <a href="">
-                    <i className="fa-regular fa-heart"></i>
-                </a>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className={`d-flex flex-row justify-content-start container-fluid ${styles.songInfoContainer}`}
+    >
+      {name && (
+        <Fragment>
+          <img src={thumbnail} alt="" />
+          <div className={`d-flex flex-column ${styles.infoCancionContainer}`}>
+            <a href="">{name}</a>
+            <a href="">{artist}</a>
+          </div>
+          <div className={`d-flex flex-column ${styles.likeContainer}`}>
+            <a href="">
+              <i className="fa-regular fa-heart"></i>
+            </a>
+          </div>
+        </Fragment>
+      )}
+    </div>
+  );
 }

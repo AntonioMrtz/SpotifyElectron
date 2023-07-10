2f9ef5c4906f9a885fd6376638c94667e27187ed
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-07-10 15:25:24 +0200

    feat : header Playlist

--- Electron/src/componentes/Playlist/Playlist.tsx
+++ Electron/src/componentes/Playlist/Playlist.tsx
@@ -1,14 +1,51 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-
+import Global from 'global/global';
+import styles from './playlist.module.css';
 
 export default function Playlist() {
-
+  /* Get current Playlist Name */
   const location = useLocation();
-  const currentURL = location.pathname;
+  let playlistName = decodeURIComponent(location.pathname.split('/').slice(-1)[0]);
 
+  const [thumbnail, setThumbnail] = useState<string>('');
+
+  useEffect(() => {
+    fetch(Global.backendBaseUrl + 'playlists/' + playlistName)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setThumbnail(res['photo']);
+      })
+      .catch((error) => {
+        console.log('No se puedo obtener la playlist');
+      });
+  }, []);
 
   return (
-    <div>{currentURL}</div>
-  )
+    <div
+      className={`d-flex container-fluid flex-column ${styles.wrapperPlaylist}`}
+    >
+      <div
+        className={`container-fluid d-flex ${styles.backgroundFilter} ${styles.header}`}
+        style={{ backgroundImage: `url(${thumbnail})` }}
+      >
+        <div className={`d-flex flex-row ${styles.nonBlurred}`}>
+          <div className={``}>
+            <img className="img-fluid" src={thumbnail} alt="" />
+          </div>
+
+          <div
+            className={`d-flex container-fluid flex-column flex-grow-1 ${styles.headerText}`}
+          >
+            <h2>Álbum</h2>
+            <h1>{playlistName}</h1>
+            <p>Info de la canción</p>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  );
 }
--- Electron/src/renderer/App.tsx
+++ Electron/src/renderer/App.tsx
@@ -7,6 +7,7 @@ import { Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 import Playlist from 'componentes/Playlist/Playlist';
 
+
 function App() {
   const [songName, setSongName] = useState('none');
 

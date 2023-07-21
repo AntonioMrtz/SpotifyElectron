25addcac108e5a41a8a8a8b117031957b0811a58
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-07-21 19:30:16 +0200

    feat : UpdatePlaylistModal desplegado y avance inputs

--- Electron/src/componentes/Playlist/Playlist.tsx
+++ Electron/src/componentes/Playlist/Playlist.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Global from 'global/global';
 import styles from './playlist.module.css';
@@ -6,6 +6,9 @@ import Song from './Song/Song';
 import { PropsSongs } from 'componentes/Sidebar/types/propsSongs.module';
 import { FastAverageColor } from 'fast-average-color';
 import defaultThumbnailPlaylist from '../../assets/imgs/DefaultThumbnailPlaylist.jpg';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 interface PropsPlaylist {
   changeSongName: Function;
@@ -20,7 +23,6 @@ export default function Playlist(props: PropsPlaylist) {
     location.pathname.split('/').slice(-1)[0]
   );
 
-
   const [thumbnail, setThumbnail] = useState<string>('');
   const [numberSongs, setNumberSongs] = useState<number>(0);
   const [songs, setSongs] = useState<PropsSongs[]>();
@@ -29,7 +31,12 @@ export default function Playlist(props: PropsPlaylist) {
     fetch(encodeURI(Global.backendBaseUrl + 'playlists/dto/' + playlistName))
       .then((res) => res.json())
       .then(async (res) => {
-        setThumbnail(res['photo'] === '' ? defaultThumbnailPlaylist : res['photo']);
+        setThumbnail(
+          res['photo'] === '' ? defaultThumbnailPlaylist : res['photo']
+        );
+        setThumbnailUpdatePlaylist(
+          res['photo'] === '' ? defaultThumbnailPlaylist : res['photo']
+        );
         if (res['song_names']) {
           setNumberSongs(res['song_names'].length);
           let propsSongs: PropsSongs[] = [];
@@ -44,9 +51,11 @@ export default function Playlist(props: PropsPlaylist) {
               refreshPlaylistData: loadPlaylistData,
             };
 
-            let artistName = await fetch(Global.backendBaseUrl + 'canciones/dto/' + obj)
+            let artistName = await fetch(
+              Global.backendBaseUrl + 'canciones/dto/' + obj
+            )
               .then((res) => res.json())
-              .then((res) => res["artist"]);
+              .then((res) => res['artist']);
 
             propsSong['artistName'] = artistName;
             propsSongs.push(propsSong);
@@ -84,6 +93,29 @@ export default function Playlist(props: PropsPlaylist) {
     fac.destroy();
   }, [thumbnail]);
 
+  /* Handle Update Playlist Data */
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '524px',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [open, setOpen] = useState(false);
+  const [thumbnailUpdatePlaylist, setThumbnailUpdatePlaylist] = useState('');
+
+  const handleOpenUpdatePlaylistModal = () => {
+    setOpen(true);
+  };
+
+  const handleUpdatePlaylist = (event: FormEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <div
       className={`d-flex container-fluid flex-column ${styles.wrapperPlaylist}`}
@@ -93,7 +125,10 @@ export default function Playlist(props: PropsPlaylist) {
         style={{ backgroundColor: `${mainColorThumbnail}` }}
       >
         <div className={`d-flex flex-row container-fluid ${styles.nonBlurred}`}>
-          <button className={`${styles.wrapperThumbnail}`}>
+          <button
+            onClick={handleOpenUpdatePlaylistModal}
+            className={`${styles.wrapperThumbnail}`}
+          >
             <img className="" src={`${thumbnail}`} alt="" />
           </button>
 
@@ -149,6 +184,83 @@ export default function Playlist(props: PropsPlaylist) {
             })}
         </ul>
       </div>
+
+      {/* Modal */}
+
+      <Modal
+        className={``}
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+        aria-labelledby="modal-modal-confirmation"
+        aria-describedby="modal-modal-confirmation-description"
+      >
+        <Box sx={style} className={`${styles.wrapperUpdatePlaylistModal}`}>
+          <header
+            className={`d-flex flex-row justify-content-between align-items-center`}
+          >
+            <h1>Editar información</h1>
+            <button
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              <i className="fa-solid fa-xmark"></i>
+            </button>
+          </header>
+
+          <form>
+            <div className={`d-flex flex-row`}>
+              <div className={` ${styles.wrapperUpdateThumbnail}`}>
+                <img src={`${thumbnailUpdatePlaylist}`} alt="" />
+              </div>
+
+              <div className={` ${styles.wrapperUpdateTextData}`}>
+                <TextField
+                  id="outlined-basic"
+                  label={'Nombre'}
+                  variant="filled"
+                  defaultValue={playlistName}
+                  style={{width:'100%'}}
+
+/*                   style={{width:'100%',height:'100%'}}
+ */                  sx={{
+                    '& .MuiFormLabel-root': {
+                      color: 'var(--primary-white)',
+                    },
+                  }}
+                />
+                <TextField
+                  id="filled-basic"
+                  label={'Añade una descripción original'}
+                  variant="filled"
+                  style={{width:'100%',height:'100%'}}
+                  multiline
+                  rows={4}
+                  sx={{
+                    '& .MuiFormLabel-root': {
+                      color: 'var(--secondary-white)',
+                    },
+                    '& .MuiInputBase-root':{
+
+                      padding:0,
+                      height:'100%'
+
+                    }
+                  }}
+                />
+              </div>
+            </div>
+
+            <div
+              className={`d-flex justify-content-end ${styles.wrapperUpdateButton}`}
+            >
+              <button onClick={handleUpdatePlaylist}>Guardar</button>
+            </div>
+          </form>
+        </Box>
+      </Modal>
     </div>
   );
 }

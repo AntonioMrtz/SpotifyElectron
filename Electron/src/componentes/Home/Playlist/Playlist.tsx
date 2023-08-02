075aed5a6e88ca853d518806cdf2589a8bb62738
Fernando <fernando.morell.teec@gmail.com>
2023-08-02 21:28:56 +0200

    style: added play button to playlists

--- Electron/src/componentes/Home/Playlist/Playlist.tsx
+++ Electron/src/componentes/Home/Playlist/Playlist.tsx
@@ -1,12 +1,46 @@
 import styles from './playlistCss.module.css';
 import { PropsPlaylist } from '../types/propsPlaylist.module';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Home(props: PropsPlaylist) {
+
+  const [displayPlay, setdisplayPlay] = useState(styles.displayTruePlay);
+  const [displayPause, setdisplayPause] = useState(styles.displayNonePlay);
+  const [Playing, setPlaying] = useState(false);
+
   let urlPlaylist = '/playlist/' + props.name;
+
+  const handlePlay = (): void => {
+    if (Playing == false) {
+      setdisplayPause(styles.displayTruePlay);
+      setdisplayPlay(styles.displayNonePlay);
+      setPlaying(true);
+    } else {
+      setdisplayPlay(styles.displayTruePlay);
+      setdisplayPause(styles.displayNonePlay);
+      setPlaying(false);
+    }
+  };
 
   return (
     <span className={`rounded ${styles.card}`}>
+    <button
+           className={`${styles.hoverablePlayButton} ${displayPlay}`}
+           onClick={handlePlay}
+         >
+           <i
+             className={`fa-solid fa-circle-play ${styles.playButton}`}
+           ></i>
+          </button>
+          <button
+           className={`${styles.hoverablePlayButton} ${displayPause}`}
+           onClick={handlePlay}
+         >
+           <i
+             className={`fa-solid fa-circle-pause ${styles.playButton}`}
+           ></i>
+    </button>
       <Link to={urlPlaylist} key={props.name}>
         <img src={props.photo} className={`card-img-top rounded`} />
         <div className={`${styles.cardBody}`}>

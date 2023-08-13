484752f391c1a374f354fabd41b09524d081903a
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-08-13 22:57:03 +0200

    style : Lint fix GenreOption

--- Electron/src/componentes/Sidebar/ModalAddSongPlaylist/Accordion/GenreOption/GenreOption.tsx
+++ Electron/src/componentes/Sidebar/ModalAddSongPlaylist/Accordion/GenreOption/GenreOption.tsx
@@ -1,22 +1,11 @@
-import { useState, useEffect } from 'react';
 import styles from '../addSongPlayListAccordion.module.css';
 
 interface PropsGenreOption {
-  name: any;
-  value: any;
+  name: string;
+  value: string;
 }
 
-export default function GenreOption(props: PropsGenreOption) {
-  const [name, setName] = useState<string>('');
-  const [value, setValue] = useState<string>('');
-
-  useEffect(() => {
-    if (props.name && props.value) {
-      setName(props.name);
-      setValue(props.value);
-    }
-  }, [props]);
-
+export default function GenreOption({ name, value }: PropsGenreOption) {
   return (
     <option className={` ${styles.option}`} value={value}>
       {name}

7e56cfd7efa53d0cdd46a8722a59609466f5bee4
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-08-23 00:40:30 +0200

    feat : add api_test playlist

--- Backend API/src/__tests__/api_test.py
+++ Backend API/src/__tests__/api_test.py
@@ -5,6 +5,8 @@ from fastapi.testclient import TestClient
 client = TestClient(app)
 
 
+# * PLAYLIST
+
 def create_playlist(name: str, descripcion: str, foto: str):
 
     url = f"/playlists/?nombre={name}&foto={foto}&descripcion={descripcion}"
@@ -20,7 +22,28 @@ def create_playlist(name: str, descripcion: str, foto: str):
 
 def get_playlist(name: str):
 
-    response = client.get(f"/playlists/dto/{name}")
+    response = client.get(f"/playlists/{name}")
+    return response
+
+
+def update_playlist(
+        name: str,
+        descripcion: str,
+        foto: str,
+        nuevo_nombre: str = ""):
+
+    if nuevo_nombre == "":
+        url = f"/playlists/{name}/?foto={foto}&descripcion={descripcion}"
+
+    else:
+        url = f"/playlists/{name}/?foto={foto}&descripcion={descripcion}&nuevo_nombre={nuevo_nombre}"
+
+    payload = []
+
+    response = client.put(
+        url, json=payload, headers={"Content-Type": "application/json"}
+    )
+
     return response
 
 
@@ -28,6 +51,8 @@ def delete_playlist(name: str):
     response = client.delete(f"/playlists/{name}")
     return response
 
+
+# * SONG
 
 def create_song(
         name: str,
@@ -47,11 +72,19 @@ def create_song(
         return response
 
 
-def get_song(name: str):
-    response = client.get(f"/canciones/dto/{name}")
+def delete_song(name: str):
+    response = client.delete(f"/canciones/{name}")
     return response
 
 
-def delete_song(name: str):
-    response = client.delete(f"/canciones/{name}")
+# * DTO
+
+def get_playlist_dto(name: str):
+
+    response = client.get(f"/playlists/dto/{name}")
+    return response
+
+
+def get_song_dto(name: str):
+    response = client.get(f"/canciones/dto/{name}")
     return response

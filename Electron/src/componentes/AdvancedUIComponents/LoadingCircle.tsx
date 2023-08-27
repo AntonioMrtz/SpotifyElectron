ebfae7c089304255100f982fe2d33ae3ca0508ae
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-08-27 12:09:32 +0200

    refactor : extract Circle loading into component

---
+++ Electron/src/componentes/AdvancedUIComponents/LoadingCircle.tsx
@@ -0,0 +1,27 @@
+import CircularProgress from '@mui/material/CircularProgress/CircularProgress';
+
+export default function LoadingCircle() {
+  return (
+    <div
+      className="container-fluid d-flex justify-content-center align-content-center"
+      style={{
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '5%',
+      }}
+    >
+      <CircularProgress
+        style={{ width: '2rem', height: 'auto' }}
+        sx={{
+          ' & .MuiCircularProgress-circle': {
+            color: 'var(--pure-white)',
+          },
+          '& .css-zk81sn-MuiCircularProgress-root': {
+            width: '3rem',
+          },
+        }}
+      />
+    </div>
+  );
+}

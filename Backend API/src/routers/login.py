224f8a4f7e0fe9b66deeb1295c04dfbf40daccab
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-09-26 21:35:00 +0200

    feat : add secret key signing on .env

--- Backend API/src/routers/login.py
+++ Backend API/src/routers/login.py
@@ -6,10 +6,14 @@ from jose import JWTError, jwt
 from datetime import datetime, timedelta
 import services.login_service as login_service
 import json
+import os
+from dotenv import load_dotenv
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = os.getenv("SECRET_KEY_SIGN")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+load_dotenv()
 
 router = APIRouter(
     prefix="/login",

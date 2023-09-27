57925713f50b906ebacba3ae5289a0fcaf161cb1
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-09-27 13:24:18 +0200

    feat : test whoami with jwt

--- Backend API/src/routers/login.py
+++ Backend API/src/routers/login.py
@@ -1,5 +1,5 @@
 from fastapi.responses import Response
-from fastapi import APIRouter,Security, Depends
+from fastapi import APIRouter,Security, Depends , Header , HTTPException , status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from typing import Annotated, Union
 from jose import JWTError, jwt
@@ -8,6 +8,9 @@ import services.login_service as login_service
 import json
 import os
 from dotenv import load_dotenv
+from dataclasses import dataclass
+from services.user_service import get_user
+
 
 SECRET_KEY = os.getenv("SECRET_KEY_SIGN")
 ALGORITHM = "HS256"
@@ -15,10 +18,19 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 load_dotenv()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
 router = APIRouter(
     prefix="/login",
     tags=["login"],
 )
+
+@dataclass
+class TokenData():
+    username: str
+    role: str
+    token_type : str
 
 
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
@@ -30,6 +42,28 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("access_token")
+        role: str = payload.get("role")
+        token_type: str = payload.get("token_type")
+        if username is None or role is None or token_type is None:
+            raise credentials_exception
+        token_data = TokenData(username=username,role=role,token_type=token_type)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(name=username)
+    if user is None:
+        raise credentials_exception
+    return user
 
 
 
@@ -60,5 +94,18 @@ def login_usuario(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) ->
 
     access_token_json = json.dumps(access_token)
 
+
+
     return Response(access_token_json, media_type="application/json", status_code=200)
 
+
+@router.get("/", tags=["login"])
+def whomai(authorization: Annotated[Union[str, None], Header()] = None) -> Response:
+
+    if authorization is None:
+            raise HTTPException(status_code=401, detail="Authorization header is missing")
+
+    user = get_current_user(authorization)
+    usuario_json = user.get_json()
+
+    return Response(usuario_json, media_type="application/json", status_code=200)

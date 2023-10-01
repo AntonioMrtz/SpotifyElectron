75e9c217396c0919fca4094ad277384dfe4ae72a
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-10-01 15:50:49 +0200

    feat : add env example & update ev mongo_pass to mongo_password

--- Backend API/src/database/Database.py
+++ Backend API/src/database/Database.py
@@ -44,7 +44,7 @@ class Database(metaclass=DatabaseMeta):
     def __init__(self):
         if Database.connection is None:
             try:
-                password = os.getenv("MONGO_PASS")
+                password = os.getenv("MONGO_PASSWORD")
                 uri = 'mongodb+srv://arso:' + \
                     str(password) + \
                     '@cluster0.ktobcwq.mongodb.net/?retryWrites=true&w=majority'

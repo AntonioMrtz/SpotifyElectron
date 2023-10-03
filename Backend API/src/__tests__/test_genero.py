d795d9f367cf98baa07f1854e7413ea937c6e3cf
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-10-03 18:55:48 +0200

    test : fix genre endpoints tests  work with middleware

--- Backend API/src/__tests__/test_genero.py
+++ Backend API/src/__tests__/test_genero.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
-import logging
+from test_API.api_token import get_user_jwt_header
+from test_API.api_test_artist import create_artist,delete_artist
 
 from main import app as app
 
@@ -7,5 +8,18 @@ client = TestClient(app)
 
 
 def test_get_generos_correct():
-    response = client.get(f"/generos/")
+
+    artista = "artista"
+    foto = "https://foto"
+    password = "hola"
+
+    res_create_artist = create_artist(name=artista,password=password,photo=foto)
+    assert res_create_artist.status_code == 201
+
+    jwt_headers = get_user_jwt_header(username=artista,password=password)
+
+    response = client.get(f"/generos/",headers=jwt_headers)
     assert response.status_code == 200
+
+    res_delete_artist = delete_artist(artista)
+    assert res_delete_artist.status_code == 202

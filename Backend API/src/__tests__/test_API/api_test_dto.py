ea89f4f187b5bde7b1d71754eff5771f1c0e4a95
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-10-04 09:27:13 +0200

    tests : update dto test with jwt middleware

--- Backend API/src/__tests__/test_API/api_test_dto.py
+++ Backend API/src/__tests__/test_API/api_test_dto.py
@@ -5,12 +5,12 @@ from fastapi.testclient import TestClient
 client = TestClient(app)
 
 
-def get_playlist_dto(name: str):
+def get_playlist_dto(name: str, headers: dict):
 
-    response = client.get(f"/playlists/dto/{name}")
+    response = client.get(f"/playlists/dto/{name}", headers=headers)
     return response
 
 
-def get_song_dto(name: str):
-    response = client.get(f"/canciones/dto/{name}")
+def get_song_dto(name: str, headers: dict):
+    response = client.get(f"/canciones/dto/{name}", headers=headers)
     return response

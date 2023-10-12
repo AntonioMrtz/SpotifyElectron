1b735f75e24d832edb3b0b44aef2968a594616f0
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-10-12 18:01:08 +0200

    test : increase coverage Home test with mock fetchs

--- Electron/src/__tests__/Home.test.tsx
+++ Electron/src/__tests__/Home.test.tsx
@@ -3,6 +3,8 @@ import { act, render } from '@testing-library/react';
 import * as router from 'react-router';
 import '@testing-library/jest-dom/extend-expect';
 import Home from 'pages/Home/Home';
+import Global from 'global/global';
+import { BrowserRouter } from 'react-router-dom';
 
 const navigate = jest.fn();
 
@@ -39,5 +41,71 @@ test('Home fields', async () => {
 
   expect(component.queryByText('Especialmente para ti')).toBeInTheDocument();
 
+  expect(component.queryByText('Artistas destacados')).toBeInTheDocument();
+});
+
+test('prueba', async () => {
+  global.fetch = jest.fn((url: string) => {
+    console.log(
+      '🚀 ~ file: Home.test.tsx:14 ~ global.fetch=jest.fn ~ url:',
+      url
+    );
+    if (url === `${Global.backendBaseUrl}artistas/`) {
+      console.log('ENTRO EN ARTISTAS');
+      return Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            artists: [
+              JSON.stringify({
+                name: 'name',
+                photo: 'photo',
+                register_date: 'date',
+                password: 'pass',
+                playback_history: [],
+                playlists: [],
+                saved_playlists: [],
+              }),
+            ],
+          }),
+        status: 200,
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+    if (url === `${Global.backendBaseUrl}playlists/`) {
+      console.log('ENTRO EN PLAYLIST');
+      return Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            playlists: [
+              JSON.stringify({
+                name: 'name',
+                photo: 'photo',
+                description: 'description',
+                upload_date: 'date',
+                owner: 'owner',
+                songs: [],
+              }),
+            ],
+          }),
+        status: 200,
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+
+    // In case the URL doesn't match, return a rejected promise
+    return Promise.reject(new Error('Unhandled URL in fetch mock'));
+  }) as jest.Mock;
+
+  const component = await act(() => {
+    return render(
+      <BrowserRouter>
+        <Home refreshSidebarData={jest.fn()} />
+      </BrowserRouter>
+    );
+  });
+
+  expect(component.queryByText('Especialmente para ti')).toBeInTheDocument();
   expect(component.queryByText('Artistas destacados')).toBeInTheDocument();
 });

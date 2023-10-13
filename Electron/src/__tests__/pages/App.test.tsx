3c870ecbeaefedd7e5b6c722a3f87ae8361db575
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-10-13 16:54:30 +0200

    fix : add mock scrollto in App tests

--- Electron/src/__tests__/pages/App.test.tsx
+++ Electron/src/__tests__/pages/App.test.tsx
@@ -4,6 +4,9 @@ import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../../renderer/App';
 
+const scrollToMock = jest.fn();
+Object.defineProperty(window, 'scrollTo', { value: scrollToMock });
+
 describe('App', () => {
   it('should render', () => {
     expect(

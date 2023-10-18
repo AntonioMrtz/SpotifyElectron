887f9c2f00ddabc8bbe0e6c65441d3a04643815e
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-10-18 11:15:18 +0200

    test : add Player tests

--- Electron/src/components/footer/Player/Player.tsx
+++ Electron/src/components/footer/Player/Player.tsx
@@ -221,7 +221,6 @@ export default function Player({
     /* Pause audio if component unmount */
     return () => {
       if (audio.current) {
-        console.log('ENTROPAUSE');
         audio.current.pause();
         handlePause();
       }

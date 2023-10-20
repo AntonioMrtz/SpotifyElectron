ef70aaf1cc9dcac0cf82b91081eb5ae7cbb87c6b
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-10-21 00:01:09 +0200

    feat : add workflow generate docs

---
+++ Backend API/src/generate-docs.py
@@ -0,0 +1,36 @@
+"""Script to export the ReDoc documentation page into a standalone HTML file."""
+
+import json
+
+from main import app
+
+HTML_TEMPLATE = """<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>My Project - ReDoc</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="https://fastapi.tiangolo.com/img/favicon.png">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+    <style data-styled="" data-styled-version="4.4.1"></style>
+</head>
+<body>
+    <div id="redoc-container"></div>
+    <script src="https://cdn.jsdelivr.net/npm/redoc/bundles/redoc.standalone.js"> </script>
+    <script>
+        var spec = %s;
+        Redoc.init(spec, {}, document.getElementById("redoc-container"));
+    </script>
+</body>
+</html>
+"""
+
+if __name__ == "__main__":
+    with open(f"api-docs-spotify-electron.html", "w") as fd:
+        print(HTML_TEMPLATE % json.dumps(app.openapi()), file=fd)

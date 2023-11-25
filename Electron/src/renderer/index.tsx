1a3e51e271d6a1c816fef6195c340443d56c87d1
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-11-25 11:38:34 +0100

    fix : replace browser router with hashrouter for build

--- Electron/src/renderer/index.tsx
+++ Electron/src/renderer/index.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );

d8287a0d389d9bf39ac9a7ac93b4ebc45f5f48b6
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2024-02-18 21:40:55 +0100

    chore : use local backend in frontend requests

--- Electron/src/global/global.ts
+++ Electron/src/global/global.ts
@@ -1,6 +1,5 @@
 namespace Global {
-  export const backendBaseUrl: string =
-    process.env.BACKEND_URL || 'no-backend-provided';
+  export const backendBaseUrl: string = 'http://127.0.0.1:8000/';
 
   export const repositoryUrl: string =
     'https://github.com/AntonioMrtz/SpotifyElectron/';
@@ -12,5 +11,4 @@ namespace Global {
     canGoForward: boolean | undefined;
   }
 }
-
 export default Global;

bb8d6d822ad8d574277e48c1e6c13d57431d5bc8
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2024-04-20 11:49:39 +0200

    chore : minor adjustments http encode service

--- Backend/app/services/http_encode_service.py
+++ Backend/app/services/http_encode_service.py
@@ -4,7 +4,6 @@ from typing import Any
 from fastapi.encoders import jsonable_encoder
 
 from app.exceptions.http_encode_exceptions import JsonEncodeException
-from app.logging.http_encode_logging_constants import ENCODING_ERROR
 from app.logging.logger_constants import LOGGING_HTTP_ENCODE_SERVICE
 from app.logging.logging_schema import SpotifyElectronLogger
 
@@ -31,19 +30,14 @@ def get_json(object: Any) -> str:
 
 
 def get_json_with_iterable_field(object: Any, field_name: str) -> str:
-    """Returns json string from an object inside an object with a field name
+    """Returns a json string that contains an object inside a field name
 
     Args:
-        object (Any): the object thats going to be converted into json string
-        field_name (str): the name of the field that will contain the object\
-              in the dictionary
-
-    Raises:
-        JsonEncodeException: if an error occurred while encoding the object \
-            into json string
+        object (Any): the object to be put inside a field name
+        field_name (str): the name of the field name where the object will be put on
 
     Returns:
-        str: the object converted into json string inside an object with a field name
+        str: the json string with the object inside a field name
     """
 
     object_dict = {field_name: object}
@@ -69,7 +63,8 @@ def object_to_json(object: Any) -> str:
         http_encode_service_logger.debug(
             f"Success encoding object into json : {json_object}"
         )
-        return json_object
-    except Exception as error:
-        http_encode_service_logger.error(f"{ENCODING_ERROR}: {error}")
+    except Exception:
+        http_encode_service_logger.exception("Error encoding object into json")
         raise JsonEncodeException()
+    else:
+        return json_object

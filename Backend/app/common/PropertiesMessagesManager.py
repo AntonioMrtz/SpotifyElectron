23fb1002218f8c1f9e18d0464df9c76615ef28ec
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2024-04-26 21:10:59 +0200

    feat : add PropertiesMessagesManager

---
+++ Backend/app/common/PropertiesMessagesManager.py
@@ -0,0 +1,71 @@
+import configparser
+import os
+import re
+
+from app.exceptions.exceptions_schema import SpotifyElectronException
+from app.logging.logger_constants import LOGGING_PROPERTIES_MESSAGES_MANAGER
+from app.logging.logging_schema import SpotifyElectronLogger
+
+PROPERTIES_INI_FILE_PATH = "../resources/messages.ini"
+
+properties_messages_manager_logger = SpotifyElectronLogger(
+    LOGGING_PROPERTIES_MESSAGES_MANAGER
+).getLogger()
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROPERTIES_INI_FILE_PATH = os.path.join(CURRENT_DIR, "../resources/messages.ini")
+
+
+class _PropertiesMessagesManager:
+    def __init__(self):
+        # Load properties from file
+        config = configparser.ConfigParser()
+        config.optionxform = (
+            lambda option: option
+        )  # <-- agregamos esta línea para mantener las mayúsculas
+        config.read(PROPERTIES_INI_FILE_PATH)
+
+        # Loop through all sections and options to set properties dynamically
+        for section in config.sections():
+            for option in config.options(section):
+                value = str(config[section][option])
+
+                # Convert option to camelCase with dots
+                option = re.sub(
+                    r"\.([a-z])", lambda match: match.group(1).upper(), option
+                )
+                setattr(self, option, value)
+
+    def __iter__(self):
+        pass
+
+    def __getattr__(self, name) -> None:
+        # Check if attribute exists with original name
+        try:
+            return object.__getattribute__(self, name)
+        except AttributeError:
+            pass
+
+        # Convert attribute name to camelCase with dots
+        new_name = re.sub(r"\.([a-z])", lambda match: match.group(1).upper(), name)
+
+        # Check if attribute exists with new name
+        try:
+            return object.__getattribute__(self, new_name)
+        except AttributeError as exception:
+            properties_messages_manager_logger.exception(
+                f"'{self.__class__.__name__}' object has no attribute '{name}'"
+            )
+            raise MessageNotFoundException from exception
+
+
+class MessageNotFoundException(SpotifyElectronException):
+    """Exception for message not found in PropertiesMessagesManager"""
+
+    ERROR = "Error getting message from PropertiesMessagesManager, it doesnt exists"
+
+    def __init__(self):
+        super().__init__(self.ERROR)
+
+
+PropertiesMessagesManager = _PropertiesMessagesManager()

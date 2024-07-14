c3fdc43da2459ccb200dea83b3227a284a496418
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2024-07-14 17:22:17 +0200

    refactor: add type hints for testing apis

--- Backend/tests/test_API/api_login.py
+++ Backend/tests/test_API/api_login.py
@@ -1,10 +1,11 @@
 from fastapi.testclient import TestClient
+from httpx import Response
 
 from app.__main__ import app
 
 client = TestClient(app)
 
 
-def post_login(user_name: str, password: str):
+def post_login(user_name: str, password: str) -> Response:
     data = {"username": user_name, "password": password}
     return client.post("/login", data=data)

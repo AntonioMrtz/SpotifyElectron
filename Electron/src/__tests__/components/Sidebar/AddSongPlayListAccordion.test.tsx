523219e51494a7530fe67f3b9ef4632397892039
Martin Perignáth <martin.perignath@student.tuke.sk>
2024-10-05 18:46:37 +0200

    test: split valid and invalid file upload tests | [#239]

--- Electron/src/__tests__/components/Sidebar/AddSongPlayListAccordion.test.tsx
+++ Electron/src/__tests__/components/Sidebar/AddSongPlayListAccordion.test.tsx
@@ -131,7 +131,7 @@ test('AddSongPlaylistAccordion submit playlist correct', async () => {
   expect(component.getByText('Playlist Añadida')).toBeInTheDocument();
 });
 
-test('AddSongPlaylistAccordion submit song correct', async () => {
+test('AddSongPlaylistAccordion submit valid song files', async () => {
   const handleCloseMock = jest.fn();
   const refreshSidebarDataMock = jest.fn();
   const setIsCloseAllowed = jest.fn();
@@ -192,10 +192,122 @@ test('AddSongPlaylistAccordion submit song correct', async () => {
     'sidebar-file-input',
   ) as HTMLInputElement;
 
-  // Create a sample files
+  // Create valid sample files
   const mp3File = new File(['(⌐□_□)'], 'sample.mp3', { type: 'audio/mp3' });
   const wavFile = new File(['(⌐□_□)'], 'sample.wav', { type: 'audio/wav' });
-  const flacFile = new File(['(⌐□_□)'], 'sample.wav', { type: 'audio/wav' });
+  const flacFile = new File(['(⌐□_□)'], 'sample.flac', { type: 'audio/flac' });
+  const aacFile = new File(['(⌐□_□)'], 'sample.aac', { type: 'audio/aac' });
+
+  await act(async () => {
+    fireEvent.change(inputName, {
+      target: { value: 'testuser' },
+    });
+    fireEvent.change(inputPhoto, {
+      target: { value: 'testpassword' },
+    });
+    /* ! TODO For some reasion both fireEvent are necessary . Without one
+    the genre selected is empty causing the test to fail
+     */
+    fireEvent.change(selectGenreOption, {
+      target: { value: 'Rock' },
+    });
+    fireEvent.change(dropdown, { target: { value: 'Rock' } });
+  });
+
+  // Test valid file formats
+  await act(async () => {
+    fireEvent.change(fileInputElement, { target: { files: [mp3File] } });
+  });
+  expect(fileInputElement.files?.[0]).toBe(mp3File);
+
+  await act(async () => {
+    fireEvent.change(fileInputElement, { target: { files: [wavFile] } });
+  });
+  expect(fileInputElement.files?.[0]).toBe(wavFile);
+
+  await act(async () => {
+    fireEvent.change(fileInputElement, { target: { files: [flacFile] } });
+  });
+  expect(fileInputElement.files?.[0]).toBe(flacFile);
+
+  await act(async () => {
+    fireEvent.change(fileInputElement, { target: { files: [aacFile] } });
+  });
+  expect(fileInputElement.files?.[0]).toBe(aacFile);
+
+  const submitSongButton = component.getByTestId(
+    'sidebar-addsongplaylistaccordion-submit-song',
+  );
+
+  await act(async () => {
+    fireEvent.click(submitSongButton);
+  });
+
+  expect(component.getByText('Canción Añadida')).toBeInTheDocument();
+});
+
+test('AddSongPlaylistAccordion rejects invalid song files', async () => {
+  const handleCloseMock = jest.fn();
+  const refreshSidebarDataMock = jest.fn();
+  const setIsCloseAllowed = jest.fn();
+
+  global.fetch = jest.fn(async (url: string) => {
+    if (url === `${Global.backendBaseUrl}/genres/`) {
+      return Promise.resolve({
+        json: () => JSON.stringify({ Rock: 'Rock', Pop: 'Pop' }),
+        status: 200,
+        ok: true,
+        headers: getMockHeaders(),
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+
+    return Promise.resolve({
+      json: () => {},
+      status: 201,
+      ok: true,
+      headers: getMockHeaders(),
+    }).catch((error) => {
+      console.log(error);
+    });
+  }) as jest.Mock;
+
+  const component = await act(() => {
+    return render(
+      <BrowserRouter>
+        <AddSongPlayListAccordion
+          handleClose={handleCloseMock}
+          refreshSidebarData={refreshSidebarDataMock}
+          setIsCloseAllowed={setIsCloseAllowed}
+        />
+      </BrowserRouter>,
+    );
+  });
+
+  const accordionExpandSong = component.getByTestId(
+    'accordion-expand-submit-song',
+  );
+
+  await act(async () => {
+    fireEvent.click(accordionExpandSong);
+  });
+
+  expect(component.getByText('Subir canción')).toBeInTheDocument();
+
+  const inputName = component.getByPlaceholderText('Nombre de la canción');
+  const inputPhoto = component.getByPlaceholderText(
+    'URL de la miniatura de la canción',
+  );
+  const selectGenreOption = component.getByText('❗ Elige un género');
+  const dropdown = component.getByTestId('select-genre');
+
+  // Find the file input element by it's name or other suitable selector
+  const fileInputElement = component.getByTestId(
+    'sidebar-file-input',
+  ) as HTMLInputElement;
+
+  // Create an invalid sample file
   const invalidFile = new File(['a'], 'sample.txt', { type: 'text/plain' });
 
   await act(async () => {
@@ -214,23 +326,7 @@ test('AddSongPlaylistAccordion submit song correct', async () => {
     fireEvent.change(dropdown, { target: { value: 'Rock' } });
   });
 
-  // Valid files
-  await act(async () => {
-    fireEvent.change(fileInputElement, { target: { files: [mp3File] } });
-  });
-  expect(fileInputElement.files?.[0]).toBe(mp3File);
-
-  await act(async () => {
-    fireEvent.change(fileInputElement, { target: { files: [wavFile] } });
-  });
-  expect(fileInputElement.files?.[0]).toBe(wavFile);
-
-  await act(async () => {
-    fireEvent.change(fileInputElement, { target: { files: [flacFile] } });
-  });
-  expect(fileInputElement.files?.[0]).toBe(flacFile);
-
-  // Invalid file
+  // Test invalid file format
   await act(async () => {
     fireEvent.change(fileInputElement, { target: { files: [invalidFile] } });
   });
@@ -244,5 +340,5 @@ test('AddSongPlaylistAccordion submit song correct', async () => {
     fireEvent.click(submitSongButton);
   });
 
-  expect(component.getByText('Canción Añadida')).toBeInTheDocument();
+  expect(component.queryByText('Canción Añadida')).not.toBeInTheDocument();
 });

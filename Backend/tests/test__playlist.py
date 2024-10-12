652b89a287ae8c5b2d0662d7b8057e31f66d7457
Antonio Martínez Fernández <antoniomartinezfernandez17@gmail.com>
2024-10-12 14:51:34 +0200

    fix: solve add/remove songs playlist overwrite content by adding standalone methods | [#235]
    * refactor: add songs to playlist and remove from it into standalone methods | [#235]
    
    * test: add tests for add/remove songs in playlist | [#235]
    
    * chore: update openapi client | [#235]
    
    * chore: remove  duplicated test health | [#235]
    
    * chore: remove Singleton and pattern folder | [#235]
    
    * chore: remove unsused JWT Middleware | [#235]

--- Backend/tests/test__playlist.py
+++ Backend/tests/test__playlist.py
@@ -14,12 +14,15 @@ from starlette.status import (
 from tests.test_API.api_base_users import patch_playlist_saved
 from tests.test_API.api_test_artist import create_artist
 from tests.test_API.api_test_playlist import (
+    add_songs_to_playlist,
     create_playlist,
     delete_playlist,
     get_playlist,
     get_playlists,
+    remove_song_from_playlist,
     update_playlist,
 )
+from tests.test_API.api_test_song import create_song, delete_song
 from tests.test_API.api_test_user import delete_user, get_user
 from tests.test_API.api_token import get_user_jwt_header
 
@@ -278,4 +281,281 @@ def test_get_playlists():
     assert res_delete_playlist.status_code == HTTP_202_ACCEPTED
 
     res_delete_artist = delete_user(owner)
+    assert res_delete_artist.status_code == HTTP_202_ACCEPTED
+
+
+def test_add_songs_to_playlist_correct():
+    song_name = "song-name"
+    song_name_2 = "song-name-2"
+    song_name_3 = "song-name-3"
+    file_path = "tests/assets/song.mp3"
+    genre = "Pop"
+    photo = "https://photo"
+
+    playlist_name = "playlist-name"
+    playlist_owner = "playlist-user"
+    description = "playlist-description"
+    password = "password"
+
+    songs = [song_name, song_name_2, song_name_3]
+
+    res_create_artist = create_artist(playlist_owner, photo, password)
+    assert res_create_artist.status_code == HTTP_201_CREATED
+
+    jwt_headers = get_user_jwt_header(username=playlist_owner, password=password)
+
+    res_create_playlist = create_playlist(
+        name=playlist_name, descripcion=description, photo=photo, headers=jwt_headers
+    )
+    assert res_create_playlist.status_code == HTTP_201_CREATED
+
+    for name in songs:
+        res_create_song = create_song(
+            name=name,
+            file_path=file_path,
+            genre=genre,
+            photo=photo,
+            headers=jwt_headers,
+        )
+        assert res_create_song.status_code == HTTP_201_CREATED
+
+    res_add_song_to_playlist = add_songs_to_playlist(
+        name=playlist_name, song_names=[song_name, song_name_3], headers=jwt_headers
+    )
+    assert res_add_song_to_playlist.status_code == HTTP_204_NO_CONTENT
+
+    expected_number_playlist_songs = 2
+
+    res_get_playlist = get_playlist(playlist_name, headers=jwt_headers)
+    assert res_get_playlist.status_code == HTTP_200_OK
+    assert len(res_get_playlist.json()["song_names"]) == expected_number_playlist_songs
+
+    res_add_song_to_playlist = add_songs_to_playlist(
+        name=playlist_name, song_names=[song_name_2], headers=jwt_headers
+    )
+    assert res_add_song_to_playlist.status_code == HTTP_204_NO_CONTENT
+
+    expected_number_playlist_songs = 3
+
+    res_get_playlist = get_playlist(playlist_name, headers=jwt_headers)
+    assert res_get_playlist.status_code == HTTP_200_OK
+    assert len(res_get_playlist.json()["song_names"]) == expected_number_playlist_songs
+
+    for name in songs:
+        res_delete_song = delete_song(name)
+        assert res_delete_song.status_code == HTTP_202_ACCEPTED
+
+    res_delete_playlist = delete_playlist(playlist_name)
+    assert res_delete_playlist.status_code == HTTP_202_ACCEPTED
+
+    res_delete_artist = delete_user(playlist_owner)
+    assert res_delete_artist.status_code == HTTP_202_ACCEPTED
+
+
+def test_add_songs_to_playlist_playlist_not_found():
+    song_name = "song-name"
+    file_path = "tests/assets/song.mp3"
+    genre = "Pop"
+    photo = "https://photo"
+
+    playlist_name = "playlist-name"
+    playlist_owner = "playlist-user"
+    password = "password"
+
+    res_create_artist = create_artist(playlist_owner, photo, password)
+    assert res_create_artist.status_code == HTTP_201_CREATED
+
+    jwt_headers = get_user_jwt_header(username=playlist_owner, password=password)
+
+    res_create_song = create_song(
+        name=song_name,
+        file_path=file_path,
+        genre=genre,
+        photo=photo,
+        headers=jwt_headers,
+    )
+    assert res_create_song.status_code == HTTP_201_CREATED
+
+    res_add_song_to_playlist = add_songs_to_playlist(
+        name=playlist_name, song_names=[song_name], headers=jwt_headers
+    )
+    assert res_add_song_to_playlist.status_code == HTTP_404_NOT_FOUND
+
+    res_delete_song = delete_song(song_name)
+    assert res_delete_song.status_code == HTTP_202_ACCEPTED
+
+    res_delete_artist = delete_user(playlist_owner)
+    assert res_delete_artist.status_code == HTTP_202_ACCEPTED
+
+
+def test_add_songs_to_playlist_song_not_found():
+    song_name = "song-name"
+    photo = "https://photo"
+
+    playlist_name = "playlist-name"
+    playlist_owner = "playlist-user"
+    description = "playlist-description"
+    password = "password"
+
+    res_create_artist = create_artist(playlist_owner, photo, password)
+    assert res_create_artist.status_code == HTTP_201_CREATED
+
+    jwt_headers = get_user_jwt_header(username=playlist_owner, password=password)
+
+    res_create_playlist = create_playlist(
+        name=playlist_name, descripcion=description, photo=photo, headers=jwt_headers
+    )
+    assert res_create_playlist.status_code == HTTP_201_CREATED
+
+    res_add_song_to_playlist = add_songs_to_playlist(
+        name=playlist_name, song_names=[song_name], headers=jwt_headers
+    )
+    assert res_add_song_to_playlist.status_code == HTTP_404_NOT_FOUND
+
+    res_delete_playlist = delete_playlist(playlist_name)
+    assert res_delete_playlist.status_code == HTTP_202_ACCEPTED
+
+    res_delete_artist = delete_user(playlist_owner)
+    assert res_delete_artist.status_code == HTTP_202_ACCEPTED
+
+
+def test_remove_song_from_playlist_correct():
+    song_name = "song-name"
+    song_name_2 = "song-name-2"
+    song_name_3 = "song-name-3"
+    file_path = "tests/assets/song.mp3"
+    genre = "Pop"
+    photo = "https://photo"
+
+    playlist_name = "playlist-name"
+    playlist_owner = "playlist-user"
+    description = "playlist-description"
+    password = "password"
+
+    songs = [song_name, song_name_2, song_name_3]
+
+    res_create_artist = create_artist(playlist_owner, photo, password)
+    assert res_create_artist.status_code == HTTP_201_CREATED
+
+    jwt_headers = get_user_jwt_header(username=playlist_owner, password=password)
+
+    res_create_playlist = create_playlist(
+        name=playlist_name, descripcion=description, photo=photo, headers=jwt_headers
+    )
+    assert res_create_playlist.status_code == HTTP_201_CREATED
+
+    for name in songs:
+        res_create_song = create_song(
+            name=name,
+            file_path=file_path,
+            genre=genre,
+            photo=photo,
+            headers=jwt_headers,
+        )
+        assert res_create_song.status_code == HTTP_201_CREATED
+
+    res_add_song_to_playlist = add_songs_to_playlist(
+        name=playlist_name,
+        song_names=[song_name, song_name_2, song_name_3],
+        headers=jwt_headers,
+    )
+    assert res_add_song_to_playlist.status_code == HTTP_204_NO_CONTENT
+
+    res_remove_song_to_playlist = remove_song_from_playlist(
+        name=playlist_name, song_names=[song_name, song_name_2], headers=jwt_headers
+    )
+    assert res_remove_song_to_playlist.status_code == HTTP_202_ACCEPTED
+
+    expected_number_playlist_songs = 1
+
+    res_get_playlist = get_playlist(playlist_name, headers=jwt_headers)
+    assert res_get_playlist.status_code == HTTP_200_OK
+    assert len(res_get_playlist.json()["song_names"]) == expected_number_playlist_songs
+
+    res_remove_song_to_playlist = remove_song_from_playlist(
+        name=playlist_name, song_names=[song_name_3], headers=jwt_headers
+    )
+    assert res_remove_song_to_playlist.status_code == HTTP_202_ACCEPTED
+
+    expected_number_playlist_songs = 0
+
+    res_get_playlist = get_playlist(playlist_name, headers=jwt_headers)
+    assert res_get_playlist.status_code == HTTP_200_OK
+    assert len(res_get_playlist.json()["song_names"]) == expected_number_playlist_songs
+
+    for name in songs:
+        res_delete_song = delete_song(name)
+        assert res_delete_song.status_code == HTTP_202_ACCEPTED
+
+    res_delete_playlist = delete_playlist(playlist_name)
+    assert res_delete_playlist.status_code == HTTP_202_ACCEPTED
+
+    res_delete_artist = delete_user(playlist_owner)
+    assert res_delete_artist.status_code == HTTP_202_ACCEPTED
+
+
+def test_remove_song_from_playlist_playlist_not_found():
+    song_name = "song-name"
+    file_path = "tests/assets/song.mp3"
+    genre = "Pop"
+    photo = "https://photo"
+
+    playlist_name = "playlist-name"
+    playlist_owner = "playlist-user"
+    password = "password"
+
+    res_create_artist = create_artist(playlist_owner, photo, password)
+    assert res_create_artist.status_code == HTTP_201_CREATED
+
+    jwt_headers = get_user_jwt_header(username=playlist_owner, password=password)
+
+    res_create_song = create_song(
+        name=song_name,
+        file_path=file_path,
+        genre=genre,
+        photo=photo,
+        headers=jwt_headers,
+    )
+    assert res_create_song.status_code == HTTP_201_CREATED
+
+    res_remove_song_from_playlist = remove_song_from_playlist(
+        name=playlist_name, song_names=[song_name], headers=jwt_headers
+    )
+    assert res_remove_song_from_playlist.status_code == HTTP_404_NOT_FOUND
+
+    res_delete_song = delete_song(song_name)
+    assert res_delete_song.status_code == HTTP_202_ACCEPTED
+
+    res_delete_artist = delete_user(playlist_owner)
+    assert res_delete_artist.status_code == HTTP_202_ACCEPTED
+
+
+def test_remove_song_from_playlist_song_not_found():
+    song_name = "song-name"
+    photo = "https://photo"
+
+    playlist_name = "playlist-name"
+    playlist_owner = "playlist-user"
+    description = "playlist-description"
+    password = "password"
+
+    res_create_artist = create_artist(playlist_owner, photo, password)
+    assert res_create_artist.status_code == HTTP_201_CREATED
+
+    jwt_headers = get_user_jwt_header(username=playlist_owner, password=password)
+
+    res_create_playlist = create_playlist(
+        name=playlist_name, descripcion=description, photo=photo, headers=jwt_headers
+    )
+    assert res_create_playlist.status_code == HTTP_201_CREATED
+
+    res_remove_song_from_playlist = remove_song_from_playlist(
+        name=playlist_name, song_names=[song_name], headers=jwt_headers
+    )
+    assert res_remove_song_from_playlist.status_code == HTTP_404_NOT_FOUND
+
+    res_delete_playlist = delete_playlist(playlist_name)
+    assert res_delete_playlist.status_code == HTTP_202_ACCEPTED
+
+    res_delete_artist = delete_user(playlist_owner)
     assert res_delete_artist.status_code == HTTP_202_ACCEPTED

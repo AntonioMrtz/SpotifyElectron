465f2a7f6461c1e22aaa6c4b96a9cd5627486d0f
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2024-10-29 17:49:47 +0100

    refactor: use boostrap npm package instead injected html scripts | [#273]

--- Electron/src/renderer/index.tsx
+++ Electron/src/renderer/index.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+// Import bootstrap before app and custom styles so own code it's not override
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Only needed for dynamic components
 import App from './App';
 import './index.css';
 import initOpenAPIClient from '../swagger/openAPIClientInit';

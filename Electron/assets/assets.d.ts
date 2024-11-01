7caea5cfb24c29e020a4efed5a26649d87f8f93e
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2024-11-01 21:45:18 +0100

    chore: remove missing sass/scss | [#270]

--- Electron/assets/assets.d.ts
+++ Electron/assets/assets.d.ts
@@ -19,16 +19,6 @@ declare module '*.jpg' {
   export default content;
 }
 
-declare module '*.scss' {
-  const content: Styles;
-  export default content;
-}
-
-declare module '*.sass' {
-  const content: Styles;
-  export default content;
-}
-
 declare module '*.css' {
   const content: Styles;
   export default content;

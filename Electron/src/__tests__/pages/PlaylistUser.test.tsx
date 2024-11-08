9c3fb82cda2cee57df1842debbc4e8ed67000800
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2024-11-08 18:53:01 +0100

    style: fix lint and format issues | [#267]

--- Electron/src/__tests__/pages/PlaylistUser.test.tsx
+++ Electron/src/__tests__/pages/PlaylistUser.test.tsx
@@ -522,7 +522,9 @@ test('Playlist user role update playlist', async () => {
 
 test('Playlist updates song name in context when a song is clicked', async () => {
   global.fetch = jest.fn((url: string) => {
-    if (url === `${Global.backendBaseUrl}/artists/${artistMockFetch.name}/songs`) {
+    if (
+      url === `${Global.backendBaseUrl}/artists/${artistMockFetch.name}/songs`
+    ) {
       return Promise.resolve({
         json: () => artistMockFetch,
         status: 200,
@@ -532,7 +534,10 @@ test('Playlist updates song name in context when a song is clicked', async () =>
         console.log(error);
       });
     }
-    if (url === `${Global.backendBaseUrl}/users/${userMockFetch.name}/playback_history`) {
+    if (
+      url ===
+      `${Global.backendBaseUrl}/users/${userMockFetch.name}/playback_history`
+    ) {
       return Promise.resolve({
         json: () => userMockFetch,
         status: 200,

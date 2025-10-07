b052035fa64fc1403170386803009213d4f90452
Vidha Singh Chauhan <vidha.vsc@gmail.com>
2025-10-07 10:36:32 +0530

    feat: add more unit tests

--- Backend/tests/test_audio_utils.py
+++ Backend/tests/test_audio_utils.py
@@ -11,14 +11,22 @@ from app.spotify_electron.utils.audio.audio_utils import check_file_size, check_
 
 
 file = open(r"C:\Users\admin\Desktop\all projects\SpotifyElectron\Backend\tests\assets\song_4_seconds.mp3", "rb")
-fake = r"this is fake audio byte file"
+fake = r"this is fake audio byte file" # fake byte file for testing
+empty = r"" # empty file for testing
 
 text = file.read()
 
 """
 CHECK FILE SIZE
 
+--- test1 ---
 if check_file_size(text):
+    print("file size is okay")
+else:
+    print("file size is too big")
+
+--- test2 ---
+if check_file_size(fake):
     print("file size is okay")
 else:
     print("file size is too big")
@@ -29,7 +37,14 @@ else:
 """
 CHECK FILE TYPE
 
+--- test1 ---
 if check_file_type(text):
+    print("file type is audio, nothing malicious")
+else:
+    print("nope, file type is not audio")
+
+--- test2 ---
+if check_file_type(fake):
     print("file type is audio, nothing malicious")
 else:
     print("nope, file type is not audio")
@@ -40,6 +55,7 @@ else:
 """
 TEST strip_metadata()
 
+--- test1 ---
 original_audio = mutagen.File(io.BytesIO(text))
 print("Original metadata:", original_audio.tags)
 
@@ -48,18 +64,37 @@ cleaned_bytes = strip_metadata(file)
 cleaned_audio = mutagen.File(io.BytesIO(cleaned_bytes))
 print("After stripping:", cleaned_audio.tags)
 
+--- test2 ---
+original_audio = mutagen.File(io.BytesIO(empty))
+print("Original metadata:", original_audio.tags)
+
+cleaned_bytes = strip_metadata(file)
+
+cleaned_audio = mutagen.File(io.BytesIO(cleaned_bytes))
+print("After stripping:", cleaned_audio.tags)
+
+
 """
 
 
 """
 TEST reencode_audio()
 
+--- test1 ---
 reencoded_bytes = reencode_audio(text)
 
-if check_file_type(reencoded_bytes):
-    print("file type is audio, nothing malicious")
+if puremagic.from_string(stripped_metadata_bytes).lower() != ".mp3":
+    print("file reencoded")
 else:
-    print("nope, file type is not audio")
+    print("nope, file could not reencode")
+
+--- test1 ---
+reencoded_bytes = reencode_audio(empty)
+
+if puremagic.from_string(stripped_metadata_bytes).lower() != ".mp3":
+    print("file reencoded")
+else:
+    print("nope, file could not reencode")
 
 """
 
